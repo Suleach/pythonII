c995e90e18c2b240c964d50f526cd7a74473bb88
Suleach <suleach@ya.ru>
2020-09-10 22:16:31 +0600

    123

---
+++ 2_lecture/substitution.py
@@ -0,0 +1,26 @@
+alpha_eng = "qwertyuiop[]asdfghjkl;'zxcvbnm,."
+alpha_rus = "йцукенгшщзхъфывапролджэячсмитьбю"
+
+def encode(text):
+    encode = ""
+    for i in range(len(text)):
+        for j in range(len(alpha_eng)):
+            if text[i]==alpha_eng[j]:
+                encode = encode + alpha_rus[j]
+    return encode
+
+def decode(text):
+    decode = ""
+    for i in range(len(text)):
+        for j in range(len(alpha_rus)):
+            if text[i]==alpha_rus[j]:
+                decode = decode + alpha_eng[j]
+    return decode
+
+text = str(input("Enter text: "))
+
+if text[0] in alpha_rus:
+    right = decode(text)
+if text[0] in alpha_eng:
+    right = encode(text)
+print(right)
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-import random
-print(random.randint(0,5))
+alpha_eng = "qwertyuiop[]asdfghjkl;'zxcvbnm,. "
+alpha_rus = "йцукенгшщзхъфывапролджэячсмитьбю "
+text = "руддщ"
+encode = ""
+for i in range(len(text)):
+    for j in range(len(alpha_rus)):
+        if text[i]==alpha_rus[j]:
+            encode = encode + alpha_eng[j]
+print(encode)
