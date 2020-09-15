626f0b9da424706591d541d90a8dd6a64652bd9f
Suleach <suleach@ya.ru>
2020-09-15 20:23:30 +0600

    123

--- 2_lecture/word.py
+++ 2_lecture/word.py
@@ -2,19 +2,53 @@
 import random
 
 list_of_words = ['simple', 'list','of', 'words']
-the_word = list_of_words[random.randint(0,5)]
+the_word = list_of_words[random.randint(0,3)]
 game_over = False
 board = list("*" * len(the_word))
 it_was = []
 points = [0,0,0]
 turn = 1
+baraban = [100,200,300,400,500, 600,700, 800, 900, 1000,"priz"]
+prizes = [
+    "Automobil",
+    "Putyovka",
+    "Gazonokosilka",
+    "Cheese",
+    "Tapochki"
+]
 while not game_over:
+    spin = random.randint(0, 10)
     print("")
     print("------------------------------")
     print(f"Turn Player {turn}")
     print(f"Player 1: {points[1]} points")
     print(f"Player 2: {points[2]} points")
     print(f"Guess a word: {' '.join(board)}")
+
+    if type(baraban[spin]) == str:
+        print("Sector priz on baraban")
+        priz = prizes[random.randint(0,4)]
+        money = 1000
+        money_list = [250, 500, 750, 1000, 2000 ,3000]
+        get_prize = False
+        cnt=0
+        amount = random.randint(3,7)
+        while not get_prize:
+            print(f"I wanna give you {money}!")
+            choice = str(input(" What you goona take?(p/m): "))
+            if choice =="p":
+                cnt += 1
+                money += money_list[random.randint(0,5)]
+                if cnt == amount:
+                    print(f"Congratulations, your prize is {priz}.")
+                    get_prize = True
+            else:
+                print(f"Congratulations, you win {money} dollars")
+                get_prize = True                   
+    else:
+        bet = baraban[spin]
+        print(f"Bet is {bet}")
+
     user_guess = input("Enter a word or a letter: ")
     user_guess = user_guess.lower()
 
@@ -23,11 +57,12 @@ while not game_over:
         if user_guess in it_was:
             print(f'Word {user_guess} was try another')
             continue
+        
         for i in range(len(the_word)):
             if the_word[i] == user_guess:
                 board[i] = user_guess
                 no_letter = 0
-                points[turn] += 10
+                points[turn] += bet
         if no_letter:
             print(f'No letter {user_guess} in this word')
             if turn == 1:
--- test.py
+++ test.py
@@ -1,43 +1,4 @@
-trans_dict = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "г": "g",
-    "д": "d",
-    "е": "ye",
-    "ё": "yo",
-    "ж": "zh",
-    "з": "z",
-    "и": "i",
-    "й": "iy",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "kh",
-    "ц": "еы",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "shch",
-    "ъ": "",
-    "ы": "y",
-    "ь": "'",
-    "э": "e",
-    "ю": "yu",
-    "я": "ya",
-    " ": " "
-}
-text = str(input('Enter word or text: ')).lower()
-text = list(text)
-result = ''
-
-for i in text:
-    result = result + trans_dict[i]
-print(result)
+import numpy as np
+import random
+a= [1, 'hell']
+print(a[1])
